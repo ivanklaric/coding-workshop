f4e4c2aed74197ccf2cc45f6789974c5a61d31aa
Ivan Klaric <iklaric@clarity.hr>
2025-03-22 12:05:09 +0100

    added mario skeleton

--- lab-8/mario_lib.py
+++ lab-8/mario_lib.py
@@ -6,3 +6,26 @@ def ucitaj_sliku(filename, size=(40, 30)):
 
 def nacrtaj_sliku(screen, x, y, slika):
     screen.blit(slika, (x, y))
+
+def inicijaliziraj_polje(broj_redaka, broj_stupaca):
+    polje = []
+    for _ in range(broj_redaka):
+        red = [0] * broj_stupaca
+        polje.append(red)
+    return polje
+
+def napravi_ravnicu(polje):
+    donji_redak = polje[-1]
+    for kolona in range(len(donji_redak)):
+        polje[-1][kolona] = 1
+    return polje
+
+def nacrtaj_pravkutnik_polja(screen, boja, redak, kolona):
+    pygame.draw.rect(screen, boja, ( kolona * 16, redak * 16, 16, 16 ))
+
+def nacrtaj_polje(screen, polje):
+    for broj_retka in range(len(polje)):
+        red = polje[broj_retka]
+        for broj_kolone in range(len(red)):
+            if polje[broj_retka][broj_kolone] == 1:
+                nacrtaj_pravkutnik_polja(screen, 'brown', broj_retka, broj_kolone)
--- lab-8/super_mario.py
+++ lab-8/super_mario.py
@@ -2,7 +2,12 @@ import pygame
 import sys
 from mario_lib import *
 
-SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
+SCREEN_WIDTH, SCREEN_HEIGHT = 800, 512
+BROJ_REDAKA, BROJ_STUPACA = 32, 50
+
+polje = inicijaliziraj_polje(BROJ_REDAKA, BROJ_STUPACA)
+polje = napravi_ravnicu(polje)
+polje[10][10] = 1
 
 pygame.init()
 font = pygame.font.SysFont('Arial', 24)
@@ -21,6 +26,7 @@ while running:
     score_text = font.render('Score: ' + str(score), True, (0, 0, 0))
     screen.blit(score_text, (0,0))
 
+    nacrtaj_polje(screen, polje)
     pygame.display.flip()
 
     # Cap the frame rate
