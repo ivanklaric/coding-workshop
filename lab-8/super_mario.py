20875243b5b86acc5138ba342a4305cc4968038a
Ivan Klaric <iklaric@clarity.hr>
2025-03-29 10:50:02 +0100

    Added the first mario level

--- lab-8/super_mario.py
+++ lab-8/super_mario.py
@@ -1,13 +1,13 @@
 import pygame
 import sys
 from mario_lib import *
+from mario_levels import *
 
 SCREEN_WIDTH, SCREEN_HEIGHT = 800, 512
 BROJ_REDAKA, BROJ_STUPACA = 32, 50
 
-polje = inicijaliziraj_polje(BROJ_REDAKA, BROJ_STUPACA)
-polje = napravi_ravnicu(polje)
-polje[10][10] = 1
+# polje = inicijaliziraj_polje(BROJ_REDAKA, BROJ_STUPACA)
+polje = first_level()
 
 pygame.init()
 font = pygame.font.SysFont('Arial', 24)
@@ -15,7 +15,7 @@ pygame.display.set_caption("Super Mario!")
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 score = 0
 running = True
-
+lista_slika = ucitaj_sve_slike()
 
 while running:
     # Handle events
@@ -27,7 +27,7 @@ while running:
     score_text = font.render('Score: ' + str(score), True, (0, 0, 0))
     screen.blit(score_text, (0,0))
 
-    nacrtaj_polje(screen, polje)
+    nacrtaj_polje(screen, lista_slika, polje)
     pygame.display.flip()
 
     # Cap the frame rate
