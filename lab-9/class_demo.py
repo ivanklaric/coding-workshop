7c20e438f72a389eca8493972c1ca51b7b8a6730
ivanklaric <iklaric@gmail.com>
2025-05-17 10:30:44 +0200

    new class

--- lab-9/class_demo.py
+++ lab-9/class_demo.py
@@ -1,1 +1,18 @@
-print("hello, world")
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
+    def bark(self):
+        return f"{self.name} says woof!"
+
+# Create an instance of the Dog class
+my_dog = Dog("Rex", 3)
+floki = Dog("Floki", 1)
+
+# Access attributes and methods
+print(my_dog.name)  # Rex
+print(my_dog.age) # 3
+print(my_dog.bark())  # Rex says woof!
+print(floki.bark())
+print(f"Floki is {floki.age} years old")
