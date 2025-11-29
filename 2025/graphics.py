266bae5df2aba9bc8322b8a59c0b47ecc85e36b8
ivanklaric <iklaric@gmail.com>
2025-11-29 11:30:09 +0100

    added moving graphcs

--- 2025/graphics.py
+++ 2025/graphics.py
@@ -4,26 +4,47 @@ import pygame
 pygame.init()
 
 # Create a window
-screen = pygame.display.set_mode((640, 480))
-pygame.display.set_caption("Hello Pygame!")
+screen = pygame.display.set_mode((1024, 768))
+pygame.display.set_caption("99 days!")
+
+mario_x = 0
+mario_y = 0
+speed_x = 1
+speed_y = 1
 
 # Game loop
 running = True
 while running:
+    mario_x += speed_x
+    mario_y += speed_y
+
     # Check for close event
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     
     # Fill screen with white
-    screen.fill((255, 255, 255))
+    screen.fill((43, 92, 56))
     
     # Draw "Hello World" text
     font = pygame.font.Font(None, 74)
-    text = font.render("Hello World!", True, (0, 0, 0))
-    text_rect = text.get_rect(center=(320, 240))
+    text = font.render("99 days in the woods!", True, (153, 90, 86))
+    text_rect = text.get_rect(center=(512, 384))
     screen.blit(text, text_rect)
+
+    image = pygame.image.load('mario.png')
+    screen.blit(image, (mario_x, mario_y))
     
+    if mario_x > 1024 - image.get_rect().width:
+        speed_x = -1
+    if mario_y > 768 - image.get_rect().height:
+        speed_y = -1
+    if mario_x < 0:
+        speed_x = 1
+    if mario_y < 0:
+        speed_y = 1
+
+
     # Update display
     pygame.display.flip()
 
