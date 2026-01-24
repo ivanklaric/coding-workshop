9e6c826fc09a500bf70fe8307c5fc169f17a65f1
ivanklaric <iklaric@gmail.com>
2026-01-24 11:14:14 +0100

    added words

--- 2025/hangman.py
+++ 2025/hangman.py
@@ -4,6 +4,8 @@ pygame.init()
 screen = pygame.display.set_mode((600, 768))
 pygame.display.set_caption("Vjesala.")
 font = pygame.font.SysFont("Courier New", 48)
+font2 = pygame.font.SysFont("Courier New", 24)
+
 
 BACKGROUND_COLOR = (8, 56, 4)
 LINE_COLOR = (255, 0, 0)
@@ -11,29 +13,50 @@ LINE_WIDTH = 3
 FONT_COLOR = (255, 255, 255)
 
 mistake_count = 0
-secret_word = 'BUNKER'
+dictionary = [
+    "COVJEK", "DIJETE", "ZENA", "GRAD", "KUCA", "PAS", "MACKA", "DRVO", "RIJEKA", "MORE",
+    "PLANINA", "SUNCE", "MJESEC", "ZVIJEZDA", "NEBO", "ZEMLJA", "VODA", "HRANA", "KRUH", "MLIJEKO",
+    "KAVA", "CAJ", "VOCE", "POVRCE", "MESO", "RIBA", "KREMŠNITA", "TRG", "ULICA", "CESTA",
+    "AUTO", "VLAK", "AVION", "BROD", "BICIKL", "SKOLA", "KNJIGA", "OLOVKA", "PAPIR", "RACUNALO",
+    "TELEFON", "STOL", "STOLICA", "KREVET", "PROZOR", "VRATA", "SOBA", "KUHINJA", "KUPAONICA", "VRT",
+    "CVIJET", "TRAVA", "SUMA", "POLJE", "OTOK", "OBALA", "PIJESAK", "KAMEN", "ZIMA", "LJETI",
+    "PROLJECE", "JESEN", "KIŠA", "SNIJEG", "VJETAR", "OBLAK", "VATRA", "SRECA", "LJUBAV", "PRIJATELJ",
+    "OBITELJ", "MAJKA", "OTAC", "BRAT", "SESTRA", "RAD", "IGRA", "PJESMA", "GLAZBA", "FILM",
+    "SLIKA", "JEZIK", "RIJEC", "VRIJEME", "DAN", "NOC", "JUTRO", "VECER", "SAT", "Tjedan",
+    "MJESEC", "GODINA", "DRZAVA", "NAROD", "NOVAC", "POSAO", "SKOLA", "BOLNICA", "CRKVA", "ZAKON"
+]
+secret_word = 'SAMOBOR'
 guessed_letters = ['_', '_', '_', '_', '_', '_']
+attempted_letters = []
 
 running = True
+game_over = False
 while running:
     guessed_word = " ".join(guessed_letters)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.KEYDOWN:
+        if event.type == pygame.KEYDOWN and not game_over:
             user_char = event.unicode.upper()
             if user_char in secret_word:
                 for index, letter in enumerate(secret_word):
                     if letter == user_char:
                         guessed_letters[index] = user_char
+                       
             else:
-                mistake_count += 1
+                if not user_char in attempted_letters:
+                    attempted_letters.append(user_char)
+                    mistake_count += 1
 
     screen.fill( BACKGROUND_COLOR )
 
     text = font.render(guessed_word, True, FONT_COLOR)
     screen.blit(text, (100, 10))
+
+    attempted_str = ",".join(attempted_letters)
+    attempted_text = font2.render(attempted_str, True, FONT_COLOR)
+    screen.blit(attempted_text, (50, 710))
 
     # Drawing the gallows
     pygame.draw.line(screen, LINE_COLOR, (50, 700), (150, 700), LINE_WIDTH)  
@@ -63,6 +86,15 @@ while running:
     ## Right leg
     if mistake_count >= 7:
         pygame.draw.line(screen, LINE_COLOR, (300, 375), (350, 500), LINE_WIDTH)  
+        game_over_text = font.render("Game Over!", True, FONT_COLOR)
+        screen.blit(game_over_text, (150, 300))
+        game_over = True
+
+    if not "_" in guessed_letters:
+        victory_text = font.render("Victory!", True, FONT_COLOR)
+        screen.blit(victory_text, (170, 300))
+        game_over = True
+
 
     pygame.display.flip()
 
