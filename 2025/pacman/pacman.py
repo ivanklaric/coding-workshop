0b7011569a0c6599126f008449428f7389d789a7
clarityklax <iklaric@clarity.hr>
2026-05-16 10:27:42 +0200

    added the grid

--- 2025/pacman/pacman.py
+++ 2025/pacman/pacman.py
@@ -11,6 +11,13 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    # Draw the labyrinth boundaries
+    pygame.draw.line(screen, "BLUE", (9, 174), (759, 174), 2)
+    pygame.draw.line(screen, "BLUE", (9, 174), (9, 924), 2)
+    pygame.draw.line(screen, "BLUE", (9, 924), (759, 924), 2)
+    pygame.draw.line(screen, "BLUE", (759, 174), (759, 924), 2)
+
     pygame.display.flip()
 
 pygame.quit()
