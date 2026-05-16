f0e0c1458576a3240bc8a6c697fd3eee56a137b6
clarityklax <iklaric@clarity.hr>
2026-05-16 10:59:55 +0200

    added small dot

--- 2025/pacman/pacman.py
+++ 2025/pacman/pacman.py
@@ -5,18 +5,41 @@ pygame.init()
 screen = pygame.display.set_mode((768, 1024))
 pygame.display.set_caption("Pacman!")
 
+maze_element = pygame.image.load("img/maze-element.png")
+small_dot = pygame.image.load("img/small-dot.png")
+# 0 = prazno polje
+# 1 = zid
+# 2 = mala mrvica
+# 3 = velika mrvica
 
+level = [
+    (1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+    (1, 2, 2, 2, 2, 2, 2, 1, 2, 0, 0, 0, 0, 0, 1),
+    (1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1),
+    (1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1),
+    (1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1),
+    (1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1),
+    (1, 0, 0, 0, 1, 0, 1, 1, 1, 2, 1, 0, 0, 0, 1),
+    (1, 0, 1, 0, 0, 0, 1, 0, 1, 2, 0, 0, 1, 0, 1),
+    (1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1),
+    (1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1),
+    (1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1),
+    (1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1),
+    (1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1),
+    (1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+]
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # Draw the labyrinth boundaries
-    pygame.draw.line(screen, "BLUE", (9, 174), (759, 174), 2)
-    pygame.draw.line(screen, "BLUE", (9, 174), (9, 924), 2)
-    pygame.draw.line(screen, "BLUE", (9, 924), (759, 924), 2)
-    pygame.draw.line(screen, "BLUE", (759, 174), (759, 924), 2)
+    for r in range(15):
+        for i in range(15):
+            if level[r][i] == 1:
+                screen.blit(maze_element, (9 + 50*i, 174 +50*r))
+
 
     pygame.display.flip()
 
